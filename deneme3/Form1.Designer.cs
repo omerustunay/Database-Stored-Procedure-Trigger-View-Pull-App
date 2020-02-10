8f21089ff4b921a1cd36b17aec544b62b5f01863
160202050 <160202050@kocaeli.edu.tr>
2020-02-10 20:40:12 +0300

    proje eklendi

---
+++ deneme3/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace sp_update
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnSP = new System.Windows.Forms.Button();
+            this.lblUyari = new System.Windows.Forms.Label();
+            this.btnTrigger = new System.Windows.Forms.Button();
+            this.btnTable = new System.Windows.Forms.Button();
+            this.btnView = new System.Windows.Forms.Button();
+            this.btnFunctions = new System.Windows.Forms.Button();
+            this.btnListKarsilastirma = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(55, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Sp ismini texte taşı ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSP
+            // 
+            this.btnSP.Location = new System.Drawing.Point(55, 85);
+            this.btnSP.Name = "btnSP";
+            this.btnSP.Size = new System.Drawing.Size(128, 35);
+            this.btnSP.TabIndex = 1;
+            this.btnSP.Text = "SP";
+            this.btnSP.UseVisualStyleBackColor = true;
+            this.btnSP.Click += new System.EventHandler(this.btnSP_Click);
+            // 
+            // lblUyari
+            // 
+            this.lblUyari.AutoSize = true;
+            this.lblUyari.Location = new System.Drawing.Point(128, 59);
+            this.lblUyari.Name = "lblUyari";
+            this.lblUyari.Size = new System.Drawing.Size(0, 17);
+            this.lblUyari.TabIndex = 2;
+            // 
+            // btnTrigger
+            // 
+            this.btnTrigger.Location = new System.Drawing.Point(224, 27);
+            this.btnTrigger.Name = "btnTrigger";
+            this.btnTrigger.Size = new System.Drawing.Size(128, 31);
+            this.btnTrigger.TabIndex = 3;
+            this.btnTrigger.Text = "Trigger";
+            this.btnTrigger.UseVisualStyleBackColor = true;
+            this.btnTrigger.Click += new System.EventHandler(this.btnTrigger_Click);
+            // 
+            // btnTable
+            // 
+            this.btnTable.Location = new System.Drawing.Point(55, 149);
+            this.btnTable.Name = "btnTable";
+            this.btnTable.Size = new System.Drawing.Size(128, 34);
+            this.btnTable.TabIndex = 4;
+            this.btnTable.Text = "Table";
+            this.btnTable.UseVisualStyleBackColor = true;
+            this.btnTable.Click += new System.EventHandler(this.btnTable_Click);
+            // 
+            // btnView
+            // 
+            this.btnView.Location = new System.Drawing.Point(224, 149);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(128, 35);
+            this.btnView.TabIndex = 5;
+            this.btnView.Text = "View";
+            this.btnView.UseVisualStyleBackColor = true;
+            this.btnView.Click += new System.EventHandler(this.btnView_Click);
+            // 
+            // btnFunctions
+            // 
+            this.btnFunctions.Location = new System.Drawing.Point(224, 85);
+            this.btnFunctions.Name = "btnFunctions";
+            this.btnFunctions.Size = new System.Drawing.Size(128, 35);
+            this.btnFunctions.TabIndex = 6;
+            this.btnFunctions.Text = "Functions";
+            this.btnFunctions.UseVisualStyleBackColor = true;
+            this.btnFunctions.Click += new System.EventHandler(this.btnFunctions_Click);
+            // 
+            // btnListKarsilastirma
+            // 
+            this.btnListKarsilastirma.Location = new System.Drawing.Point(55, 207);
+            this.btnListKarsilastirma.Name = "btnListKarsilastirma";
+            this.btnListKarsilastirma.Size = new System.Drawing.Size(297, 28);
+            this.btnListKarsilastirma.TabIndex = 7;
+            this.btnListKarsilastirma.Text = "Liste Karsilastirma";
+            this.btnListKarsilastirma.UseVisualStyleBackColor = true;
+            this.btnListKarsilastirma.Click += new System.EventHandler(this.btnListKarsilastirma_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 264);
+            this.Controls.Add(this.btnListKarsilastirma);
+            this.Controls.Add(this.btnFunctions);
+            this.Controls.Add(this.btnView);
+            this.Controls.Add(this.btnTable);
+            this.Controls.Add(this.btnTrigger);
+            this.Controls.Add(this.lblUyari);
+            this.Controls.Add(this.btnSP);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Database Islemleri";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnSP;
+        private System.Windows.Forms.Label lblUyari;
+        private System.Windows.Forms.Button btnTrigger;
+        private System.Windows.Forms.Button btnTable;
+        private System.Windows.Forms.Button btnView;
+        private System.Windows.Forms.Button btnFunctions;
+        private System.Windows.Forms.Button btnListKarsilastirma;
+    }
+}
+
---
+++ deneme3/Form1.cs
@@ -0,0 +1,418 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Sdk.Sfc;
+using Microsoft.SqlServer.Management.Smo;
+using View = Microsoft.SqlServer.Management.Smo.View;
+using Table = Microsoft.SqlServer.Management.Smo.Table;
+using Trigger = Microsoft.SqlServer.Management.Smo.Trigger;
+using System.Collections.Specialized;
+
+namespace sp_update
+{
+    public partial class Form1 : Form
+    {
+        public string ConnectionString { get; private set; }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private ServerConnection serverKurulum()
+        {
+            ServerConnection serverConnection = new ServerConnection()
+            {
+                ConnectionString = ConfigurationManager.ConnectionStrings["sp_update.Properties.Settings.Setting"].ConnectionString
+
+            };
+            return serverConnection;
+        }
+
+        #region TumSPleriGetir Database-->Local
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("sp isimleri kayıt ediliyor ", "Bilgilendirme Penceresi");
+            lblUyari.Text = "Bu islem biraz zaman alabilir. Lütfen Bekleyiniz.";
+
+            string spNameFilePath = ConfigurationManager.AppSettings["spNameFile"];
+            string filePath = @spNameFilePath;
+
+            Server server = new Server(serverKurulum());
+            string dbName = ConfigurationManager.AppSettings["db"];
+            Database db = server.Databases[dbName];
+            List<SqlSmoObject> list = new List<SqlSmoObject>();
+            DataTable dataTable = db.EnumObjects(DatabaseObjectTypes.StoredProcedure);
+            foreach (DataRow row in dataTable.Rows)
+            {
+                string sSchema = (string)row["Schema"];
+                if (sSchema == "sys" || sSchema == "INFORMATION_SCHEMA")
+                    continue;
+
+                StoredProcedure sp = (StoredProcedure)server.GetSmoObject(
+                new Urn((string)row["Urn"]));
+                if (!sp.IsSystemObject)
+                    list.Add(sp);
+                string name = sp.Schema + '.' + sp.Name;
+
+                try
+                {
+                    TextWriter tw = new StreamWriter(spNameFilePath, true);
+                    tw.Write(name);
+                    tw.Write("\n");
+                    tw.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("hata var" + ex);
+
+                }
+            }
+            MessageBox.Show("sp isimleri kayıt edildi ", "Bilgilendirme Penceresi");
+            lblUyari.Text = "";
+        }
+
+        #endregion
+
+        #region SP
+        private void btnSP_Click(object sender, EventArgs e)
+        {
+
+            string spFilePath = ConfigurationManager.AppSettings["spFile"];
+            string spNameFilePath = ConfigurationManager.AppSettings["spNameFile"];
+            string filePath = @spNameFilePath;
+            List<string> spName = File.ReadAllLines(filePath).ToList();
+
+            Server server = new Server(serverKurulum());
+            string dbName = ConfigurationManager.AppSettings["db"];
+            Database db = server.Databases[dbName];
+            List<SqlSmoObject> list = new List<SqlSmoObject>();
+            DataTable dataTable = db.EnumObjects(DatabaseObjectTypes.StoredProcedure);
+            foreach (DataRow row in dataTable.Rows)
+            {
+                int count = 0;
+                string sSchema = (string)row["Schema"];
+                if (sSchema == "sys" || sSchema == "INFORMATION_SCHEMA")
+                    continue;
+
+                StoredProcedure sp = (StoredProcedure)server.GetSmoObject(
+                new Urn((string)row["Urn"]));
+                if (!sp.IsSystemObject)
+                {
+                    string name = sp.Schema + '.' + sp.Name;
+
+                    foreach (var item in spName)
+                    {
+                        if (item.Equals(name))
+                        {
+                            list.Add(sp);
+                            count++;
+                        }
+                    }
+                }
+
+                for (int i = 0; i < count; i++)
+                {
+                    try
+                    {
+                        TextWriter tw = new StreamWriter(spFilePath + sp.Schema + '.' + sp.Name + ".txt", true);
+                        tw.Write(sp.TextHeader);
+                        tw.Write("\n");
+                        tw.Write(sp.TextBody);
+                        tw.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("hata var: " + ex);
+                    }
+                }
+            }
+            MessageBox.Show("spler kayıt edildi.", "Bilgilendirme Penceresi");
+
+        }
+
+        #endregion
+
+        #region     Trigger
+        private void btnTrigger_Click(object sender, EventArgs e)
+                {
+
+
+                    string spFilePath = ConfigurationManager.AppSettings["spFile"];
+                    string spNameFilePath = ConfigurationManager.AppSettings["spNameFile"];
+                    string filePath = @spNameFilePath;
+                    List<string> spName = File.ReadAllLines(filePath).ToList();
+
+                    Server server = new Server(serverKurulum());
+                    string dbName = ConfigurationManager.AppSettings["db"];
+                    Database db = server.Databases[dbName];
+                    List<SqlSmoObject> list = new List<SqlSmoObject>();
+                    DataTable dataTable = db.EnumObjects(DatabaseObjectTypes.Table);
+
+
+                    #region trigger
+                    var allTables = dataTable.AsEnumerable()
+                                .Select(row => (Table)server.GetSmoObject(new Urn(row["Urn"].ToString())))
+                                .ToList();
+                    var triggerTables = allTables.Where(t => spName.Any(x => t.Triggers.Contains(x))).ToList();
+                    var scriptOptions = new ScriptingOptions();
+                    scriptOptions.NoCollation = true;
+
+                    foreach (var triggerTable in triggerTables)
+                    {
+                        var triggerList = triggerTable.Triggers.Cast<Trigger>().Where(t => spName.Contains(t.Name)).ToList();
+                        foreach (var trigger in triggerList)
+                        {
+                            using (TextWriter tw = new StreamWriter(spFilePath + triggerTable.Schema + '.' + trigger.Name + ".txt", true))
+                            {
+                                var script = trigger.Script(scriptOptions).Cast<string>().Where(x => !string.IsNullOrWhiteSpace(x)).ToList();
+                                script.ForEach(y =>
+                                {
+                                    tw.Write(y);
+                                    tw.Write("\nGO\n");
+                                });
+                                tw.Close();
+                            }
+                        }
+                    }
+
+                    MessageBox.Show("spler kayıt edildi.", "Bilgilendirme Penceresi");
+                    #endregion
+                }
+        #endregion 
+
+        #region Table
+        private void btnTable_Click(object sender, EventArgs e)
+        {
+            lblUyari.Text = "Bu islem biraz zaman alabilir. Lütfen Bekleyiniz.";
+
+            string spFilePath = ConfigurationManager.AppSettings["spFile"];
+            string spNameFilePath = ConfigurationManager.AppSettings["spNameFile"];
+            string filePath = @spNameFilePath;
+            List<string> spName = File.ReadAllLines(filePath).ToList();
+
+            Server server = new Server(serverKurulum());
+            string dbName = ConfigurationManager.AppSettings["db"];
+            Database db = server.Databases[dbName];
+            List<SqlSmoObject> list = new List<SqlSmoObject>();
+            DataTable dataTable = db.EnumObjects(DatabaseObjectTypes.Table);
+            
+            #region newTable
+            var list2 = dataTable.AsEnumerable()
+                        .Select(row => new
+                        {
+                            Schema = row["Schema"].ToString(),
+                            Name = row["Name"].ToString(),
+                            Urn = row["Urn"].ToString()
+                        })
+                        .Where(x => spName.Contains($"{x.Schema}.{x.Name}"))
+                        .ToList();
+            foreach (var row in list2)
+            {
+                string sSchema = row.Schema;
+                if (sSchema == "sys" || sSchema == "INFORMATION_SCHEMA")
+                    continue;
+                var scriptOptions = new ScriptingOptions();
+                scriptOptions.ExtendedProperties = true;
+                //scriptOptions.DriPrimaryKey = true;
+                scriptOptions.NoCollation = true;
+                scriptOptions.DriDefaults = true;
+
+                scriptOptions.Triggers = true;
+
+                Table sp = (Table)server.GetSmoObject(
+                new Urn(row.Urn));
+
+                var tableList = new List<Table>();
+                if (!sp.IsSystemObject)
+                {
+                    tableList.Add(sp);
+                }
+
+
+                var ss = sp.Triggers;
+                try
+                {
+                    foreach (var item in tableList)
+                    {
+                        using (TextWriter tw = new StreamWriter(spFilePath + item.Schema + '.' + item.Name + ".txt", true))
+                        {
+                            var script = item.Script(scriptOptions).Cast<string>().Where(x => !string.IsNullOrWhiteSpace(x)).ToList();
+                            script.ForEach(y =>
+                            {
+                                tw.Write(y);
+                                tw.Write("\nGO\n");
+                            });
+                            tw.Close();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("hata var: " + ex);
+                }
+            }
+            MessageBox.Show("spler kayıt edildi.", "Bilgilendirme Penceresi");
+            #endregion
+        }
+        #endregion
+
+        #region Functions
+        private void btnFunctions_Click(object sender, EventArgs e)
+        {
+            string spFilePath = ConfigurationManager.AppSettings["spFile"];
+            string spNameFilePath = ConfigurationManager.AppSettings["spNameFile"];
+            string filePath = @spNameFilePath;
+            List<string> spName = File.ReadAllLines(filePath).ToList();
+
+            Server server = new Server(serverKurulum());
+            string dbName = ConfigurationManager.AppSettings["db"];
+            Database db = server.Databases[dbName];
+            List<SqlSmoObject> list = new List<SqlSmoObject>();
+            DataTable dataTable = db.EnumObjects(DatabaseObjectTypes.UserDefinedFunction);
+            foreach (DataRow row in dataTable.Rows)
+            {
+                int count = 0;
+                string sSchema = (string)row["Schema"];
+                if (sSchema == "sys" || sSchema == "INFORMATION_SCHEMA")
+                    continue;
+
+                UserDefinedFunction sp = (UserDefinedFunction)server.GetSmoObject(
+                new Urn((string)row["Urn"]));
+                if (!sp.IsSystemObject)
+                {
+                    string name = sp.Schema + '.' + sp.Name;
+
+                    foreach (var item in spName)
+                    {
+                        if (item.Equals(name))
+                        {
+                            list.Add(sp);
+                            count++;
+                        }
+                    }
+                }
+
+                for (int i = 0; i < count; i++)
+                {
+                    try
+                    {
+                        TextWriter tw = new StreamWriter(spFilePath + sp.Schema + '.' + sp.Name + ".txt", true);
+                        tw.Write(sp.TextHeader);
+                        tw.Write("\n");
+                        tw.Write(sp.TextBody);
+                        tw.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("hata var: " + ex);
+                    }
+                }
+            }
+            MessageBox.Show("spler kayıt edildi.", "Bilgilendirme Penceresi");
+
+        }
+
+        #endregion
+
+        #region View
+        private void btnView_Click(object sender, EventArgs e)
+        {
+            string spFilePath = ConfigurationManager.AppSettings["spFile"];
+            string spNameFilePath = ConfigurationManager.AppSettings["spNameFile"];
+            string filePath = @spNameFilePath;
+            List<string> spName = File.ReadAllLines(filePath).ToList();
+
+            Server server = new Server(serverKurulum());
+            string dbName = ConfigurationManager.AppSettings["db"];
+            Database db = server.Databases[dbName];
+            List<SqlSmoObject> list = new List<SqlSmoObject>();
+            DataTable dataTable = db.EnumObjects(DatabaseObjectTypes.View);
+            foreach (DataRow row in dataTable.Rows)
+            {
+                int count = 0;
+                string sSchema = (string)row["Schema"];
+                if (sSchema == "sys" || sSchema == "INFORMATION_SCHEMA")
+                    continue;
+
+                View sp = (View)server.GetSmoObject(
+                new Urn((string)row["Urn"]));
+                if (!sp.IsSystemObject)
+                {
+                    string name = sp.Schema + '.' + sp.Name;
+
+                    foreach (var item in spName)
+                    {
+                        if (item.Equals(name))
+                        {
+                            list.Add(sp);
+                            count++;
+                        }
+                    }
+                }
+
+                for (int i = 0; i < count; i++)
+                {
+                    try
+                    {
+                        TextWriter tw = new StreamWriter(spFilePath + sp.Schema + '.' + sp.Name + ".txt", true);
+                        tw.Write(sp.TextHeader);
+                        tw.Write("\n");
+                        tw.Write(sp.TextBody);
+                        tw.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("hata var: " + ex);
+                    }
+                }
+            }
+            MessageBox.Show("spler kayıt edildi.", "Bilgilendirme Penceresi");
+
+        }
+
+        #endregion
+
+        #region ListeKarsilastirma
+        private void btnListKarsilastirma_Click(object sender, EventArgs e)
+        {
+            string filePath = ConfigurationManager.AppSettings["filePath"];
+            string fileContent = @filePath;
+            List<string> fileContentList = File.ReadAllLines(fileContent).ToList();
+
+            string filePath2 = ConfigurationManager.AppSettings["filePath2"];
+
+            string fileContent2 = @filePath2;
+            List<string> fileContentList2 = File.ReadAllLines(fileContent2).ToList();
+
+            var differenceBetweenList = fileContentList.Except(fileContentList2).ToList();
+
+            string targetFile = ConfigurationManager.AppSettings["targetFile"];
+            TextWriter tw = new StreamWriter(targetFile, true);
+
+            for (int i = 0; i < differenceBetweenList.Count; i++)
+            {
+                tw.WriteLine(differenceBetweenList[i]);
+            }
+            tw.Close();
+        }
+
+        #endregion
+    }
+
+}
